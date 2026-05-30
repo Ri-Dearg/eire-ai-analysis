5cb16d03cdaa7a306f645253b946a8bb09524953
Rory Patrick Shridan <44118951+Ri-Dearg@users.noreply.github.com>
2026-05-30 17:11:36 +0200

    refactor(scrape): ⚡️ switch to quick_check pragma

--- scrape/raw_scrape.py
+++ scrape/raw_scrape.py
@@ -66,7 +66,7 @@ def _connect(db_path: str = DB_PATH) -> sqlite3.Connection:
     try:
         conn = sqlite3.connect(db_path)
         conn.execute('PRAGMA foreign_keys = ON')
-        conn.execute('PRAGMA integrity_check')
+        conn.execute('PRAGMA quick_check')
         print(f'Connected to {db_path}')
     except sqlite3.OperationalError as e:
         print(f'Failed to connect to {db_path}: {e}')
@@ -183,6 +183,7 @@ def ingest(
         for raw_url in urls:
             outcome = _process_url(conn, session, raw_url, oid, source_feed)
             counts[outcome] += 1
+            print(f'  {outcome}: {raw_url}')
             if outcome != 'skipped':
                 time.sleep(random.uniform(*delay_range))
     finally:
@@ -197,6 +198,20 @@ def ingest(
 
 if __name__ == '__main__':
     sample = [
-        # "https://www.rte.ie/news/...",
+        'https://www.rte.ie/news/business/2026/0331/1566027-unilever-nears-deal-to-merge-foods-unit-with-mccormick/',
+        'https://www.rte.ie/news/business/2026/0331/1566035-business-post-appoints-mark-beard-as-ceo/',
+        'https://www.rte.ie/news/business/2026/0331/1566030-heathrow-airports-fees-set-to-rise-by-1/',
+        'https://www.rte.ie/news/ireland/2026/0330/1565830-my-lovely-horse-rescue/',
+        'https://www.rte.ie/news/world/2026/0330/1565833-cuba-russia-us/',
+        'https://www.rte.ie/news/business/2026/0331/1566020-mortgage-approvals-reached-almost-12-billion-in-february/',
+        'https://www.rte.ie/brainstorm/2026/0331/1566019-iran-strait-of-harmuz-defence-strategy-persian-gulf/',
+        'https://www.rte.ie/news/munster/2026/0330/1565972-cock-fighting/',
+        'https://www.rte.ie/news/ulster/2026/0331/1566011-marian-beattie-appeal/',
+        'https://www.rte.ie/news/ulster/2026/0330/1565902-schwarzenegger-belfast-honour/',
+        'https://www.rte.ie/brainstorm/2026/0324/1565008-early-education-creche-children-education-care-problems/',
+        'https://www.rte.ie/news/ireland/2026/0330/1565866-antoin-duffy-court/',
+        'https://www.rte.ie/news/dublin/2026/0331/1565981-dublin-property-prices/',
+        'https://www.rte.ie/news/middle-east/2026/0330/1565832-iran-war/',
+        'https://www.rte.ie/news/newslens/2026/0330/1566004-air-canada-ceo/',
     ]
     ingest(sample, outlet_name='rte', source_feed='sitemap')
