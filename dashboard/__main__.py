5dba1e1ff28f5318ed0f50f75344e121b3d765c3
Rory Patrick Shridan <44118951+Ri-Dearg@users.noreply.github.com>
2026-08-01 17:42:11 +0100

    feat(board): ✨ ass init, main for dashboard

---
+++ dashboard/__main__.py
@@ -0,0 +1,17 @@
+"""Streamlit Dashboard."""
+
+import logging
+
+from .app import main
+
+# LOGGING
+logger = logging.getLogger(__name__)
+
+if __name__ == '__main__':
+    # Basic logging setup.
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s %(levelname)s %(message)s',
+        datefmt='%H:%M:%S',
+    )
+    raise SystemExit(main())
